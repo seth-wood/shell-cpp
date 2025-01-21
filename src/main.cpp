d92a96d4fdcf00617e2a330c50bc6568b2c7bc60
Seth Wood <sethlwood@me.com>
2025-01-21 01:18:20 -0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
- int main() {
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -12,9 +12,7 @@
     std::string input;
     std::getline(std::cin, input);
 
-      if (input == "exit 0") {
-        break;
-      }
+    if (input == "exit 0") return 0;
 
     std::cout << input << ": command not found\n";
   }
